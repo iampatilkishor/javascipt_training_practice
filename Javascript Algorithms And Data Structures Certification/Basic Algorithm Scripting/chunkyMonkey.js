948206a6770b9d228daf94c347b1b68121534121
kishorpdev <kishor.patil@clariontechnologies.co.in>
2018-06-22 19:33:48 +0530

    "chuncky munky problem solved"

---
+++ Javascript Algorithms And Data Structures Certification/Basic Algorithm Scripting/chunkyMonkey.js
@@ -0,0 +1,22 @@
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    // Break it up.
+    var chunckyArray = [];
+    for (var i = 0; i < arr.length; i += size) {
+      chunckyArray.push(arr.slice(i, size + i));
+    }
+    return chunckyArray;
+  }
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+
+// test cases
+// chunkArrayInGroups(["a", "b", "c", "d"], 2)                    // should return [["a", "b"], ["c", "d"]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)                      // should return [[0, 1, 2], [3, 4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)                      // should return [[0, 1], [2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)                      // should return [[0, 1, 2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)                   // should return [[0, 1, 2], [3, 4, 5], [6]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)             // should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)             // should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
