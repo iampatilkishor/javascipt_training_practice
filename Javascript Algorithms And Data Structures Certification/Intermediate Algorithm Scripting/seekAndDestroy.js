3b9c84f998e300b9d97c22eb5a6b3cc21bbf7b01
kishorpdev <kishor.patil@clariontechnologies.co.in>
2018-06-27 17:36:52 +0530

    Seek and Destroy

---
+++ Javascript Algorithms And Data Structures Certification/Intermediate Algorithm Scripting/seekAndDestroy.js
@@ -0,0 +1,35 @@
+// Intermediate Algorithm Scripting: Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note
+// You have to use the arguments object.
+
+
+function destroyer(arr) {
+    // Remove all the values
+    let inArray = arguments[0];
+    let filteredArray = [];
+    inArray.map(value => {
+      let found = false;
+      for (let i = 1; i < arguments.length; i++) {
+        if (arguments[i] == value) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        filteredArray.push(value);
+      }
+  
+    });
+    return filteredArray;
+  }
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+  
+// destroyer([1, 2, 3, 1, 2, 3], 2, 3)                                     //  should return [1, 1].
+// destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)                                     //  should return [1, 5, 1].
+// destroyer([3, 5, 1, 2, 2], 2, 3, 5)                                     //  should return [1].
+// destroyer([2, 3, 2, 3], 2, 3)                                     //  should return [].
+// destroyer(["tree", "hamburger", 53], "tree", 53)                                     //  should return ["hamburger"].
+// destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan")                                     //  should return [12,92,65].
